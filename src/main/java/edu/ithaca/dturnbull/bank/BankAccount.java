cf3c0f1bdda55a7a3dd73a5e78e0565e9f1ad1c9
dougturnbull <dougturnbull@gmail.com>
2022-01-26 11:54:24 -0500

    test 123

--- src/main/java/edu/ithaca/dturnbull/bank/BankAccount.java
+++ src/main/java/edu/ithaca/dturnbull/bank/BankAccount.java
@@ -11,7 +11,7 @@ public class BankAccount {
     public BankAccount(String email, double startingBalance){
         if (isEmailValid(email)){
             this.email = email;
-            this.balance = startingBalance + 1;
+            this.balance = startingBalance;
         }
         else {
             throw new IllegalArgumentException("Email address: " + email + " is invalid, cannot create account");
